ee3aa3af7f4c803b0e8671f6a40ec30268f5812c
albin <albin.abraham1@codecraft.co.in>
2023-08-10 16:13:22 +0530

    chore:extra line

--- division.js
+++ division.js
@@ -1,7 +1,7 @@
 // This will implement the division function to division.
 /**
  * This is divide a by b. 
- * @param {number} a 
+ * @param {number} a must be nymber
  * @param {number} b 
  * @returns the result
  */
