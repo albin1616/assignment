94ad3614521697cb0482a0831327ea32bb7d406e
Rajharsh2 <raj.harsh@codecraft.co.in>
2023-08-10 15:51:27 +0530

    docs: jsdoc documentation done

--- division.js
+++ division.js
@@ -1,5 +1,10 @@
 // This will implement the division function to division.
-
+/**
+ * This is divide a by b. 
+ * @param {number} a 
+ * @param {number} b 
+ * @returns the result
+ */
 function division(a,b){
     return a/b;
 }
